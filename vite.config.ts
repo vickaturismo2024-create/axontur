9311ca1d3a8bd6455352c73f4ff3475ee579b995
pedroagueroo <aguerop47@gmail.com>
2026-06-01 20:52:17 -0300

    Remove lovable-tagger dependency and update metadata to use custom logo for og:image

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import { VitePWA } from "vite-plugin-pwa";
 import { visualizer } from "rollup-plugin-visualizer";
 
@@ -17,7 +16,6 @@ export default defineConfig(({ mode }) => ({
 
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
     visualizer({ open: true, filename: "dist/bundle-stats.html", gzipSize: true }),
     VitePWA({
       registerType: "autoUpdate",
