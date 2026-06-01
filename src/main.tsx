8070a998c5af5da77805fd4b59beb204b1b128f9
pedroagueroo <aguerop47@gmail.com>
2026-06-01 18:54:51 -0300

    chore: explicitly register PWA service worker in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,4 +5,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Register PWA service worker
+if ('serviceWorker' in navigator) {
+  import('virtual:pwa-register').then(({ registerSW }) => {
+    registerSW({ immediate: true });
+  }).catch(err => {
+    console.error('PWA Registration failed:', err);
+  });
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
